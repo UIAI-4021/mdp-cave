997dd52c0def18554faef210ffff85f1242cd4b3
Sempiternalll <100777000+Sempiternalll@users.noreply.github.com>
2023-12-08 21:42:56 +0330

    Testing Q-learn method

--- MDP.py
+++ MDP.py
@@ -2,8 +2,10 @@
 import cliff_wakling
 import numpy as np
 
+Q = np.zeros([env.observation_space.n, env.action_space.n])
+
 A = [0, 1, 2, 3]
-S = [i for i in range(36)]
+S = [i for i in range(47)]
 T = np.zeros((len(S), len(A), len(S)))
 for i in range(len(S)):
     for j in range(len(A)):
@@ -31,22 +33,33 @@ observation, info = env.reset(seed=30)
 max_iter_number = 1000
 
 policy = {}
-for i in range(0,36):
+for i in range(0,48):
     dic = {}
     for j in range(0,4):
         dic[j] = 0.25
     policy[i] = dic
+
+learning_rate = 0.1
+gamma = 0.9
+epsilon = 0.1
+num_episodes = 1000
+
+state = 36
+total_reward = 0
 for __ in range(max_iter_number):
-
-    
     # Note: .sample() is used to sample random action from the environment's action space
-
+    if np.random.uniform(0, 1) < epsilon:
+        action = env.action_space.sample()  # Explore action space
+    else:
+        action = np.argmax(Q[state, :])  # Exploit learned values
     # Choose an action (Replace this random action with your agent's policy)
-    action = choose_action(next_state, policy)
+    #action = choose_action(next_state, policy)
 
     # Perform the action and receive feedback from the environment
     next_state, reward, done, truncated, info = env.step(action)
-
+    Q[state, action] += learning_rate * (reward + gamma * np.max(Q[next_state, :]) - Q[state, action])
+    R[state, action] = reward
+    state = next_state
     if done or truncated:
         observation, info = env.reset()
 
