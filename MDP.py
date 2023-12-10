90557df041b60d067d6b4ecf8f416af6d1abcfe1
Sempiternalll <100777000+Sempiternalll@users.noreply.github.com>
2023-12-10 20:49:45 +0330

    Final version

--- MDP.py
+++ MDP.py
@@ -6,28 +6,36 @@ max_iter_number = 1000
 value_function = np.zeros(48)
 gamma = 0.9
 theta = 1e-8
-while True:
+delta = 1
+while delta >= theta:
     delta = 0
     for s in range(48):
         v = value_function[s]
         q_values = []
         for a in range(4):
-            _, next_state, reward, _ = env.P[s][a][0]
-            if next_state == 46:
-                reward = 100
-            q_values.append(reward + gamma * value_function[next_state])
+            rtmp = 0
+            for i in range(-1, 2):
+                _, next_state, reward, _ = env.P[s][(a + i) % 4][0]
+                if next_state == 47:
+                    reward = 100
+                rtmp += 1 / 3 * (reward + gamma * value_function[next_state])
+            q_values.append(rtmp)
         value_function[s] = max(q_values)
         delta = max(delta, abs(v - value_function[s]))
-    if delta < theta:
-        break
 print("Optimal Value Function:")
-print(value_function)
+for i in range(48):
+    print(i, ': ', value_function[i])
 optimal_policy = np.zeros(48, dtype=int)
 for s in range(48):
     q_values = []
     for a in range(4):
-        _, next_state, reward, _ = env.P[s][a][0]
-        q_values.append(reward + gamma * value_function[next_state])
+        rtmp = 0
+        for i in range(-1, 2):
+            _, next_state, reward, _ = env.P[s][(a + i) % 4][0]
+            if next_state == 47:
+                reward = 100
+            rtmp += 1 / 3 * (reward + gamma * value_function[next_state])
+        q_values.append(rtmp)
     optimal_policy[s] = np.argmax(q_values)
 
 state = 36
